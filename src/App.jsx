6d0c7b67a2a81a6afac231fc2c8295b69e30bd34
Underated-kartik-code <kartikalokar8@gmail.com>
2026-03-14 08:34:25 +0530

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 
 const App = () => {
 
+  const [data, setData] = useState([])
+
   const getData = async() =>{
 
    const response = await axios.get('https://jsonplaceholder.typicode.com/users')
+
+   setData(response.data)
 
    console.log(response.data)
   }
   return (
     <div>
       <button onClick={getData}> Get data</button>
-    </div>
+      <div className='container'>
+        {data.map(function(elem,idx){
+          return <div key={idx}>
+            <div className='card'>
+
+              <h2>{elem.name}</h2>
+              <p>{elem.email}</p>
+              <h3>{elem.website}</h3>
+          
+          </div>
+        </div>
+      })}
+      </div>
+    </div> 
   )
 }
 
